2d590332da4d31239406f4d71577359a89ed3c1f
Sany Yousif <sanymamdoh@yahoo.com>
2017-08-08 21:35:24 -0500

    Update 19-1-Top_2009_agent.sql

--- 19-1-Top_2009_agent.sql
+++ 19-1-Top_2009_agent.sql
@@ -1,15 +1,15 @@
 /*19-Which sales agent made the most in sales in 2009?*/
 
 select 	(Employee.FirstName || ' '|| Employee.LastName) AS SalesAgentName,  
-			Count(Invoice.Total) As TotalSales
+	Count(Invoice.Total) As TotalSales
 from 	Employee
-			Join Customer
-				on Employee.EmployeeId = Customer.SupportRepId
-			Join Invoice
-				on Invoice.CustomerId = Customer.CustomerId
+		Join Customer
+			on Employee.EmployeeId = Customer.SupportRepId
+		Join Invoice
+			on Invoice.CustomerId = Customer.CustomerId
 where 		Employee.Title = 'Sales Support Agent' and 
-			strftime('%Y', InvoiceDate) = '2009' 
+		strftime('%Y', InvoiceDate) = '2009' 
 group By 	Employee.FirstName,
-			Employee.LastName
+		Employee.LastName
 order By 	InvoiceDate asc
 limit 1
