27d9c6ef43489cd879e5aba7b5b62ba9892a6adb
Sany Yousif <sanymamdoh@yahoo.com>
2017-08-08 21:27:53 -0500

    Update 21-Sales_Agent_Customer_Count.sql

--- 21-Sales_Agent_Customer_Count.sql
+++ 21-Sales_Agent_Customer_Count.sql
@@ -1,7 +1,7 @@
 /*21-Provide a query that shows the count of customers 
 assigned to each sales agent.*/
 select 	(Employee.FirstName || ' '|| Employee.LastName) AS SalesAgentName,  
-		count(Customer.CustomerId)As NumberOfCustomers
+	count(Customer.CustomerId)As NumberOfCustomers
 from 	Employee
 		Join Customer
 			on Employee.EmployeeId = Customer.SupportRepId
